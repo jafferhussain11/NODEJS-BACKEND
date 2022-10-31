5d89307581b5715d7eb310e38b4d3c347da6b6c4
jafferhussain11 <jafferhussain11@gmail.com>
2022-10-31 16:51:31 +0530

    running server with npm nodemon package

---
+++ NODEMON_PACKAGE/routes.js
@@ -0,0 +1,61 @@
+const fs = require('fs');
+const path = require('path');
+const filePath = path.join(__dirname, "message.txt");
+//console.log('Hello from Node.js');
+const requestHandler = (req, res) => {
+
+    const url = req.url;
+    const method = req.method;
+    if (url === '/home' || url === '/') {
+
+        fs.readFile(filePath, (err, data) => {
+            if (err) {
+                console.log(err);
+            }
+            const string = data.toString();
+            res.write('<html>');
+            res.write('<head><title>My First Page</title></head>');
+            res.write(`<body>${string}</body>`);
+            res.write('<body><h1>Please fill The Form !</h1><form action = "/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></form></body>');
+            res.write('</html>');
+            return res.end();
+        });
+        
+        
+    }
+    if(url === '/message' && req.method === 'POST'){
+    
+        const body =[];
+        req.on('data',(chunk)=> {
+    
+            //console.log(chunk);
+            body.push(chunk);
+    
+        });
+        req.on('end', ()=>{
+    
+            const parsedBody = Buffer.concat(body).toString(); //text data
+            const message = parsedBody.split('=')[1];
+            fs.writeFile('message.txt', message, (err)=>{
+    
+                res.statusCode = 302;
+                res.setHeader('Location', '/');
+                return res.end();
+    
+            });
+        });
+        
+        
+    
+    }
+}
+
+//module.exports = requestHandler;
+
+// module.exports = {
+//     handler: requestHandler,
+//     someText: 'Some text'
+// };
+
+exports.handler = requestHandler;
+exports.someText = 'Some text';
