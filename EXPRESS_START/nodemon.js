92197135c2382e1301f3500fb175151f1ad47f17
jafferhussain11 <jafferhussain11@gmail.com>
2022-11-02 16:23:02 +0530

    used body parser

--- EXPRESS_START/nodemon.js
+++ EXPRESS_START/nodemon.js
@@ -1,20 +1,25 @@
 const http = require('http');
 
 const express = require('express');
-
+const bodyParser = require('body-parser');
 const app = express();
 
-app.use((req, res, next) => {
-    
-    console.log('In the middleware!');
-    next(); // Allows the request to continue to the next middleware in line
-});
+app.use(bodyParser.urlencoded({extended: false}));
 
-app.use((req, res, next) => {
+app.use('/add-product',(req, res, next) => {
     
-    console.log('In another middleware!');
-    res.send('<h1>Hello from Express!</h1>');
+    
+    res.send('<form action="/product" method ="POST"> <input type="text" name="title"><input type="number" name="size"><button type="submit">Add Product</button></form>');
     //res.send({name: 'Max', age: 29});
 });
+app.post('/product',(req, res, next) => {
 
+    console.log(req.body);
+    res.redirect('/');
+});
+app.use('/',(req, res, next) => {
+    
+    res.send('<h1>Hello from Express.js</h1>');
+
+});
 app.listen(4000);
